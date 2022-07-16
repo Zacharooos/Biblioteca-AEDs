1591761d20c084548d598a0f302b87e0acb1ae46
Zacharooos <gabrielcesarfernandescarvalho@gmail.com>
2022-07-16 12:22:07 -0300

    Header das Structs utilizadas

---
+++ struct.h
@@ -0,0 +1,40 @@
+#ifndef STRUCT_H
+
+#define STRUCT_H
+
+#endif
+
+//Biblioteca para declaração de Structs
+
+// Pading afetas Load/Save dos dados !
+
+// Sem padding
+typedef struct aluno{
+    const char nome[52];
+    const char matricula[12];
+    int id;
+    bool pendencia;
+
+} aluno;
+
+// Sem padding
+typedef struct livro{
+    char nome[101];
+    char categoria[51];
+    int ano;
+    int id;
+    bool pendencia;
+
+    int aluno_id;
+    
+} livro;
+
+// Com padding
+typedef struct recursos{
+    int id;
+    int aluno_id;
+    
+    char tipo;
+    bool estado;
+
+} recursos;
